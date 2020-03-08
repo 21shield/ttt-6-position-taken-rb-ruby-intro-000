acf575bd9768cf11bc4ef0f4f47875ad7311eedc
Netaly Ramirez <21netaly@gmail.com>
2020-03-08 16:20:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board, idx)
   if board[idx] == "" ||  board[idx] == " " ||  board[idx] == nil
     return false
+  elsif  board[idx] == 'X' || board[idx] == 'O'
+    return true
   end
 end
